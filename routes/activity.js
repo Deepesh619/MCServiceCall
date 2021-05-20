c3cdca4e3366fabb5b979d3500b460c157ed6d64
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-05-20 18:19:20 +0530

    c

--- routes/activity.js
+++ routes/activity.js
@@ -1,6 +1,6 @@
 exports.getToken = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
-    res.setHeader('Acces-Control-Allow-Origin','*');
+    //res.setHeader('Acces-Control-Allow-Origin','*');
     //res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
     //res.setHeader('Acces-Contorl-Allow-Methods','Content-Type','Authorization');
     //res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
