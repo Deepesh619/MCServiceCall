dcef1ee8cb5cde5d010197e6075faed439b0492c
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-05-20 17:27:45 +0530

    C

---
+++ app.js
@@ -0,0 +1,20 @@
+var express     = require('express');
+//var bodyParser  = require('body-parser');
+var http        = require('http');
+//var path        = require('path');
+var activity    = require('./routes/activity');
+
+var app = express();
+
+// Configure Express
+app.set('port', process.env.PORT || 3000);
+//app.use(bodyParser.raw({type: 'application/jwt'}));
+
+//app.use(express.static(path.join(__dirname, 'public')));
+// Custom Hello World Activity Routes
+app.post('/MCService/getToken/', activity.getToken );
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+//module.exports.app = app;
---
+++ routes/activity.js
@@ -0,0 +1,5 @@
+exports.getToken = function (req, res) {
+    // Data from the req and put it in an array accessible to the main app.
+    res.send(200, 'Publish');
+    console.log('Published');
+};
