5291c71c4ac3394787ec4d9c6ede5589efb4157e
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-05-20 19:03:44 +0530

    c

--- routes/activity.js
+++ routes/activity.js
@@ -1,9 +1,49 @@
+function  performRequest(endpoint,host,headers, method, data, success) {
+    var dataString = JSON.stringify(data);
+    
+    var options = {
+      host: host,
+      path: endpoint,
+      method: method,
+      headers: headers
+    };
+  
+    var req = http.request(options, function(res) {
+      res.setEncoding('utf-8');
+  
+      var responseString = '';
+  
+      res.on('data', function(data) {
+        responseString += data;
+      });
+  
+      res.on('end', function() {
+       var responseObject =  JSON.parse(responseString);
+        success(responseObject);
+      });
+    });
+    req.write(dataString);
+    req.end();
+  }
+
+  var authHost = 'mcllzpmqql69yd9kvcz1n-mj1fqy.auth.marketingcloudapis.com';
+  var authEndpoint = '/v2/token';
+  var authData = {
+    "grant_type": "client_credentials",
+    "client_id": "process.env.clientId",
+    "client_secret":"process.env.clientSecret"
+  };
+  var authHeaders = {
+    'Content-Type': 'application/json'
+  };
+  var postMethod="POST";
 exports.getToken = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
-    res.setHeader('Access-Control-Allow-Origin','*');
-    //res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    //res.setHeader('Acces-Contorl-Allow-Methods','Content-Type','Authorization');
-    //res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-    res.send(200, 'Publish1');
+    performRequest(authEndpoint,authHost,authHeaders, postMethod, authData, function(data) {
+       var accesstoken = data.access_token;
+        res.setHeader('Access-Control-Allow-Origin','*');
+    res.send(200, 'Publish1' + accesstoken);
     console.log('Published1');
+    });
+    
 };
