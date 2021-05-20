006addc8b33f8aec6189dbaa0bd048237fbe5354
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-05-20 17:43:03 +0530

    c

--- routes/activity.js
+++ routes/activity.js
@@ -1,5 +1,8 @@
 exports.getToken = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
+    res.setHeader('Acces-Control-Allow-Origin','*');
+    res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Acces-Contorl-Allow-Methods','Content-Type','Authorization');
     res.send(200, 'Publish');
     console.log('Published');
 };
